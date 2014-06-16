0d4cb4f1eb767460f20d122b8f1805bd5dc59a1d
Toby YI <tb1210582007@126.com>
2014-06-16 17:55:56 +0800

    modity main.cpp

--- QuickViewTest/main.cpp
+++ QuickViewTest/main.cpp
@@ -1,13 +1,18 @@
 #include "widget.h"
 #include <QApplication>
 #include "myquickview.h"
+#include <QtQml>
+#include "fileio.h"
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+    qmlRegisterType<FileIO, 1>("FileIO", 1, 0, "FileIO");
     MyQuickView w;
     w.resize(300,600);
     w.show();
 
+//    MyLine ww;
+//    ww.show();
     return a.exec();
 }
